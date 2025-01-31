b01eaf89f2b443a4f3eff67db064cd11b06f38fe
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-01-31 14:47:38 -0300

    upload de arquivo

--- controllers/TrabalhoController.php
+++ controllers/TrabalhoController.php
@@ -1,7 +1,9 @@
 <?php
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\UploadedFile;
 
 class TrabalhoController extends Controller
 {
@@ -12,7 +14,21 @@ class TrabalhoController extends Controller
     }
 
     public function actionCreate() {
-        return ['message' => 'ola']; 
+        $file = UploadedFile::getInstanceByName('file');
+        $jsonData = Yii::$app->request->post('data');
+        if ($file && $file->extension === 'pdf') {
+            $directory = __DIR__ . "/uploads/";
+            $filePath = $directory . $file->baseName . '.' . $file->extension;
+
+            if ($file->saveAs($filePath)) {
+                return ['message' => 'Arquivo enviado com sucesso!', 'filePath' => $filePath];
+            } else {
+                return ['error' => 'Erro ao salvar o arquivo.'];
+            }
+        }
+
+        return ['error' => 'Arquivo inválido. Apenas PDFs são permitidos.'];
+        
 
     }
 
